c3b738e148152e7212d4b86b2c8f5ad95e9209c9
han0911 <a01099249559@gmail.com>
2025-09-06 16:39:02 +0900

    heh

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,43 @@
+import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Error from "./components/Errorpage";
-import MainPage from "./components/Maingb";
-import PptPage from "./components/PPT";
 import ProtectedRoute from "./components/ProtectedRoute";
 
+const MainPage = lazy(() => import("./components/Maingb"));
+const PptPage = lazy(() => import("./components/PPT"));
+const Error = lazy(() => import("./components/Errorpage"));
+
 function App() {
-  const userEmail = sessionStorage.getItem("userEmail"); 
+  const userEmail = sessionStorage.getItem("userEmail");
   const isAllowed = !!userEmail;
+
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route
-        path="/main"
-        element={
-          <ProtectedRoute allowed={isAllowed}>
-            <PptPage />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="*" element={<Error />} />
-    </Routes>
+    <Suspense fallback={<LoadingSpinner />}>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route
+          path="/main"
+          element={
+            <ProtectedRoute allowed={isAllowed}>
+              <PptPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Suspense>
   );
 }
+const LoadingSpinner = () => {
+  return (
+    <div className="loading-container">
+      <div className="loading-spinner">
+        <div className="spinner"></div>
+      </div>
+      <p className="loading-text">페이지를 불러오는 중...</p>
+    </div>
+  );
+};
+
 
 export default App;
